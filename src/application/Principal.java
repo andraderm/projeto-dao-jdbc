7ef3dcd44f3520481bc5f6e0595b0733eed20caa
Rodrigo Andrade <rodrigom.andrade95@gmail.com>
2020-05-23 20:58:02 -0300

    Classe departamento

---
+++ src/application/Principal.java
@@ -0,0 +1,14 @@
+package application;
+
+import model.entities.Departamento;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		 
+		Departamento dep = new Departamento(1, "Eletrônicos");
+		System.out.println(dep);
+
+	}
+
+}
