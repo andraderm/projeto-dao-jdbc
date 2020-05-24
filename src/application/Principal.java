df6cd00fcc664f9b1c2dfd9757a57deb91ff42be
Rodrigo Andrade <rodrigom.andrade95@gmail.com>
2020-05-23 21:30:34 -0300

    Classe vendedor

--- src/application/Principal.java
+++ src/application/Principal.java
@@ -1,13 +1,28 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
 import model.entities.Departamento;
+import model.entities.Vendedor;
 
 public class Principal {
 
 	public static void main(String[] args) {
 		 
-		Departamento dept = new Departamento(1, "Eletrônicos");
-		System.out.println(dept);
+		Locale.setDefault(Locale.US);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
+		try {
+			
+			Departamento dept = new Departamento(1, "Eletrônicos");
+			Vendedor v1 = new Vendedor(1, "Fábio", "fabio@gmail.com", sdf.parse("08/10/1989") ,4500.00, dept);
+			System.out.println(v1);
+			
+		} catch (ParseException e) {
+			System.out.println("Erro: " + e.getMessage());
+		}
 
 	}
 
